025937f3ed9ab3445b927b5d4e6f1af3868b2695
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-03-05 13:25:16 -0700

    Remove twitter seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -11,7 +11,6 @@ function SEO({ description, lang, meta, keywords, title }) {
           siteMetadata {
             title
             description
-            author
           }
         }
       }
@@ -43,22 +42,6 @@ function SEO({ description, lang, meta, keywords, title }) {
         {
           property: `og:type`,
           content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
         },
       ]
         .concat(
