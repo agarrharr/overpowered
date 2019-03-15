6c4c01fcf4f270eae92408525b03aad74c20d512
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-03-15 12:59:26 -0600

    Style stitched checkbox

--- src/components/mat-creator.js
+++ src/components/mat-creator.js
@@ -6,8 +6,7 @@ import calculatePrice from '../utils/calculate-price.js'
 const Panel = styled.div`
   margin: 40px 0;
 
-  input[type='radio'],
-  input[type='checkbox'] {
+  input[type='radio'] {
     display: none;
   }
 
@@ -15,8 +14,7 @@ const Panel = styled.div`
     opacity: 1;
   }
 
-  input[type='radio']:checked ~ label,
-  input[type='checkbox']:checked ~ label {
+  input[type='radio']:checked ~ label {
     opacity: 1;
     background-color: #fff;
     color: #333;
@@ -24,6 +22,92 @@ const Panel = styled.div`
       stroke: #333;
     }
   }
+
+  [type='checkbox']:not(:checked),
+  [type='checkbox']:checked {
+    position: absolute;
+    opacity: 0.01;
+  }
+  [type='checkbox']:not(:checked) + label,
+  [type='checkbox']:checked + label {
+    position: relative;
+    padding-left: 4.5em;
+    cursor: pointer;
+  }
+  [type='checkbox']:not(:checked) + label:before,
+  [type='checkbox']:checked + label:before,
+  [type='checkbox']:not(:checked) + label:after,
+  [type='checkbox']:checked + label:after {
+    content: '';
+    position: absolute;
+  }
+  [type='checkbox']:not(:checked) + label:before,
+  [type='checkbox']:checked + label:before {
+    left: 0;
+    top: -0.1em;
+    width: 4em;
+    height: 1.85em;
+    background: #dddddd;
+    border-radius: 1em;
+    transition: background-color 0.2s;
+  }
+  [type='checkbox']:not(:checked) + label:after,
+  [type='checkbox']:checked + label:after {
+    width: 1.4em;
+    height: 1.4em;
+    transition: all 0.2s;
+    border-radius: 50%;
+    background: #7f8c9a;
+    top: 0.12em;
+    left: 0.24em;
+  }
+  .text {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  /* on checked */
+  [type='checkbox']:checked + label:before {
+    background: #34495e;
+  }
+  [type='checkbox']:checked + label:after {
+    background: #39d2b4;
+    left: 2.4em;
+  }
+
+  [type='checkbox']:checked + label .ui,
+  [type='checkbox']:not(:checked) + label .ui:before,
+  [type='checkbox']:checked + label .ui:after {
+    position: absolute;
+    left: 0.6em;
+    top: 0em;
+    width: 6em;
+    border-radius: 1em;
+    font-size: 0.875em;
+    font-weight: bold;
+    transition: all 0.2s;
+  }
+  [type='checkbox']:not(:checked) + label .ui:before {
+    content: 'no';
+    left: 2.35em;
+    top: 0.1em;
+    color: #7f8c9a;
+  }
+  [type='checkbox']:checked + label .ui:after {
+    content: 'yes';
+    top: 0.2em;
+    left: 0.2em;
+    color: #39d2b4;
+  }
+  [type='checkbox']:focus + label:before {
+    border: 1px dashed #777;
+    box-sizing: border-box;
+    margin-top: -1px;
+  }
+`
+
+const Checkbox = styled.div`
+  display: flex;
 `
 
 const Description = styled.p`
@@ -190,7 +274,7 @@ class MatCreator extends React.Component {
   }
 
   render() {
-    console.log(this.state.message)
+    console.log(this.state.price)
     return (
       <div>
         <Panel>
@@ -326,15 +410,18 @@ class MatCreator extends React.Component {
         )}
         {this.state.shape && this.state.shape !== 'other' && (
           <Panel>
-            <Answer>
+            <Checkbox>
               <input
                 id="stitched"
                 type="checkbox"
                 selected={this.state.isStitched}
                 onClick={this.handleStitchChange}
               />
-              <Label htmlFor="stitched">Stitched</Label>
-            </Answer>
+              <label htmlFor="stitched">
+                <span className="ui" />
+                <span className="text">Stitched</span>
+              </label>
+            </Checkbox>
           </Panel>
         )}
         {this.state.price && !isNaN(this.state.price) ? (
