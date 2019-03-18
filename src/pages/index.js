6706a82b218b9d1a166f417e40668ce9e6d570cf
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-03-18 12:25:10 -0600

    Add link to go back to Kickstarter

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,13 @@ const QuotePage = () => (
   <Layout>
     <h1>Get a Quote</h1>
     <MatCreator />
+    <a
+      href="https://www.kickstarter.com/projects/overpoweredgamemats/136550215?ref=633652&token=a0eaaea7"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Back to the Kickstarter
+    </a>
   </Layout>
 )
 
