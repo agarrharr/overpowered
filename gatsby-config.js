5749d7977d388e63b1129b7a5061b829e3f33e8a
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-03-18 15:04:09 -0600

    Fix gatsby config and use OP favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,19 +17,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Overpowered`,
+        short_name: `Overpowered`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo-white.png`,
+        include_favicon: true,
       },
     },
     'gatsby-plugin-offline',
-    resolve: `gatsby-plugin-google-analytics`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-136435486-1",
+        trackingId: 'UA-136435486-1',
       },
+    },
   ],
 }
